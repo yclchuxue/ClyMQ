4f702e7acd6699f23ed5feee996d3e6a7a198889
yclchuxue <3136165538@qq.com>
2022-11-13 13:53:03 +0800

    add some common apis

--- server/common.go
+++ server/common.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"ClyMQ/kitex_gen/api/client_operations"
 	"fmt"
 	"net"
 	"os"
@@ -40,4 +41,37 @@ func CreateFile(path string) (file *os.File, err error) {
 	file, err = os.Create(path);
 
 	return file, err
+}
+
+func GetClisArray(clis map[string]*client_operations.Client) []string {
+	var array []string
+
+	for cli_name := range clis {
+		array = append(array, cli_name)
+	}
+
+	return array
+}
+
+func CheckChangeCli(old map[string]*client_operations.Client, new []string) (reduce, add []string) {
+	for _, new_cli := range new {
+		if _, ok := old[new_cli]; !ok {  //new_cli 在old中没有
+			add = append(add, new_cli)
+		}
+	}
+
+	for old_cli := range old {
+		had := false   //不存在
+		for _, name := range new {
+			if old_cli == name {
+				had = true
+				break
+			}
+		}
+		if !had {
+			reduce = append(reduce, old_cli) 
+		}
+	}
+
+	return reduce, add
 }
