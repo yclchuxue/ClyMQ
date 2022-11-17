262eedf66e3396da1da823abb75c419b4f788793
yclchuxue <3136165538@qq.com>
2022-11-17 20:53:07 +0800

    add some rpc apis for zkserver

--- operations.thrift
+++ operations.thrift
@@ -8,9 +8,11 @@ struct PushRequest {
 }
 
 struct PushResponse {
-    1: bool ret
+    1: bool     ret
+    2: string   err   //用来回复该partition已经不在这个broker接收
 }
 
+//暂时不使用该功能
 struct PullRequest {
     1: string consumer
     2: string topic
@@ -21,6 +23,7 @@ struct PullResponse {
     1: string message
 }
 
+//consmer发送自己的host和ip,使broker连接上自己
 struct InfoRequest {
     1: string ip_port
 }
@@ -29,6 +32,7 @@ struct InfoResponse {
     1: bool ret
 }
 
+//consumer准备开始接收信息
 struct InfoGetRequest {
     1: string   cli_name
     2: string   topic_name
@@ -41,6 +45,140 @@ struct InfoGetResponse {
     1: bool  ret
 }
 
+//struct AddTopicRequest {
+//
+//}
+//
+//struct AddTopicResponse{
+//
+//}
+
+//设置某个partition接受信息的文件和队列
+struct StartGetMessageRequest {
+    1:  string topic_name
+    2:  string part_name
+    3:  string file_name
+}
+
+struct StartGetMessageResponse {
+    1: bool    ret
+}
+
+//关闭某个partition，停止接收信息，
+struct CloseGetMessageRequest{
+    1:  string topic_name
+    2:  string part_name
+    3:  string file_name
+    4:  string new_name
+}
+
+struct CloseGetMessageResponse{
+    1:  bool    ret
+}
+
+//zkserver
+struct PrepareAcceptRequest{
+    1:  string  topic_name
+    2:  string  part_name
+    3:  string  file_name
+}
+
+struct PrepareAcceptResponse{
+    1:  bool    ret
+    2:  string  err
+}
+
+struct PrepareSendRequest{
+    1:  string  topic_name
+    2:  string  part_name
+    3:  string  file_name
+    4:  i8      option
+    5:  i64     offset
+}
+
+struct PrepareSendResponse{
+    1:  bool    ret
+    2:  string  err   //若已经准备好“had_done”
+}
+
+service Server_Operations {
+    PushResponse push(1: PushRequest req)               //producer used
+    PullResponse pull(1: PullRequest req)               //
+    InfoResponse ConInfo(1: InfoRequest req)            //consumer used
+    InfoGetResponse StarttoGet(1: InfoGetRequest req)   //consumer used
+
+    //zkserver used this rpc to request broker server
+    PrepareAcceptResponse PrepareAccept(1: PrepareAcceptRequest req)
+    PrepareSendResponse PrepareSend(1: PrepareSendRequest req)
+}
+
+//broker server 将信息发送到zkserver， zkserver连接上broker server
+struct BroInfoRequest {
+    1: string broker_name
+    2: string broker_host_port
+}
+
+struct BroInfoResponse {
+    1: bool ret
+}
+
+//broker 请求恢复缓存信息
+struct BroGetConfigRequest {
+    1:  binary  propertyinfo
+}
+
+struct BroGetConfigResponse {
+    1:  bool    ret
+    2:  binary  brokerinfo
+}
+
+//producer 请求zkserver信息该发送到那个broker上
+struct ProGetBrokRequest {
+    1:  string  topic_name
+    2:  string  part_name
+}
+
+struct ProGetBrokResponse {
+    1:  bool    ret
+    2:  string  broker_host_port
+}
+
+//consumer 请求zkserver 
+struct ConStartGetBrokRequest {
+    1:  string  cli_name
+    2:  string  topic_name
+    3:  string  part_name
+    4:  i8      option
+    5:  i64     index
+}
+        //返回brokers
+struct ConStartGetBrokResponse   {
+    1:  bool    ret
+    2:  i64     size
+    3:  binary  parts
+//    3:  binary  broks
+}
+
+struct CreateTopicRequest {
+    1:  string  topic_name
+}
+
+struct CreateTopicResponse {
+    1:  bool    ret
+    2:  string  err
+}
+
+struct CreatePartRequest {
+    1:  string  topic_name
+    2:  string  part_name
+}
+
+struct CreatePartResponse {
+    1:  bool    ret
+    2:  string  err
+}
+
+//consumer订阅topic或partition，由zkserver处理
 struct SubRequest {
     1: string consumer
     2: string topic
@@ -52,60 +190,15 @@ struct SubResponse {
     1: bool ret
 }
 
-
-service Server_Operations {
-    PushResponse push(1: PushRequest req)
-    PullResponse pull(1: PullRequest req)
-    InfoResponse ConInfo(1: InfoRequest req)
-    SubResponse  Sub(1:  SubRequest  req)
-    InfoGetResponse StarttoGet(1: InfoGetRequest req)
-}
-
-struct BroInfoRequest {
-    1: string broker_name
-    2: string broker_host_port
-}
-
-struct BroInfoResponse {
-    1: bool ret
-}
-
-struct ProGetBrokRequest {
-    1:  string  topic_name
-    2:  string  part_name
-}
-
-struct ProGetBrokResponse {
-    1:  bool    ret
-    2:  string  broker_host_port
-}
-
-struct ConGetBrokRequest {
-    1:  string topic_name
-    2:  string part_name
-    3:  i8 option
-}
-
-struct ConGetBrokResponse   {
-    1:  bool    ret
-    2:  i64     size
-    3:  binary  broks
-    4:  binary  parts
-}
-
-struct BroGetConfigRequest {
-    1:  binary  propertyinfo
-}
-
-struct BroGetConfigResponse {
-    1:  bool    ret
-    2:  binary  brokerinfo
-}
-
 service ZkServer_Operations {
+    SubResponse  Sub(1:  SubRequest  req)               //consumer used
+    CreateTopicResponse CreateTopic(1: CreateTopicRequest req)
+    CreatePartResponse  CreatePart(1: CreatePartRequest req)
     BroInfoResponse BroInfo(1: BroInfoRequest req)
     ProGetBrokResponse ProGetBroker(1:  ProGetBrokRequest req)
-    ConGetBrokResponse ConGetBroker(1:  ConGetBrokRequest req)
+    ConStartGetBrokResponse ConStartGetBroker(1:  ConStartGetBrokRequest req)
+
+    //broker 用于恢复缓存的，暂时不使用
     BroGetConfigResponse BroGetConfig(1:    BroGetConfigRequest req)
 }
 
