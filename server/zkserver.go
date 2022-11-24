007b9b9bff362157b9483a56c3bd086f9ba2a28f
yclchuxue <3136165538@qq.com>
2022-11-24 22:37:04 +0800

    add some api for distributed

--- server/zkserver.go
+++ server/zkserver.go
@@ -29,14 +29,16 @@ type Info_in struct {
 	part_name  string
 	index      int64
 	option     int8
+	dupnum 	   int8
 }
 
 type Info_out struct {
 	Err           error
 	broker_name   string
 	bro_host_port string
-	part_name     string
-	index         int64
+	Ret 		  string
+	// part_name     string
+	// index         int64
 }
 
 func NewZKServer(zkinfo zookeeper.ZkInfo) *ZkServer {
@@ -54,6 +56,7 @@ func (z *ZkServer) make(opt Options) {
 	z.Brokers = make(map[string]server_operations.Client)
 }
 
+//broker连接到zkserver，zkserver将在zookeeper上监听broker的状态
 func (z *ZkServer) HandleBroInfo(bro_name, bro_H_P string) error {
 	bro_cli, err := server_operations.NewClient(z.Name, client.WithHostPorts(bro_H_P))
 	if err != nil {
@@ -67,11 +70,13 @@ func (z *ZkServer) HandleBroInfo(bro_name, bro_H_P string) error {
 	return nil
 }
 
+//producer Get Broker
 func (z *ZkServer) ProGetBroker(info Info_in) Info_out {
 	//查询zookeeper，获得broker的host_port和name，若未连接则建立连接
 	broker, block := z.zk.GetPartNowBrokerNode(info.topic_name, info.part_name)
+
 	z.mu.RLock()
-	bro_cli, ok := z.Brokers[block.BrokerName]
+	bro_cli, ok := z.Brokers[block.LeaderBroker]
 	z.mu.RUnlock()
 
 	//未连接该broker
@@ -113,12 +118,14 @@ func (z *ZkServer) CreateTopic(info Info_in) Info_out {
 	}
 }
 
+//创建Partition
 func (z *ZkServer) CreatePart(info Info_in) Info_out {
 
 	//可添加限制数量等操作
 	pnode := zookeeper.PartitionNode{
 		Name: info.part_name,
 		TopicName: info.topic_name,
+		Option: -2,
 		PTPoffset: int64(0),
 	}
 
@@ -132,6 +139,60 @@ func (z *ZkServer) CreatePart(info Info_in) Info_out {
 	err = z.CreateNowBlock(info)
 	return Info_out{
 		Err: err,
+	}
+}
+
+//设置Partition的接收信息方式
+//若ack = -1,则为raft同步信息
+//若ack = 1, 则leader写入,	fetch获取信息
+//若ack = 0, 则立即返回,   	fetch获取信息
+func (z *ZkServer)SetPartitionState(info Info_in) Info_out {
+	var ret string
+	node := z.zk.GetPartState(info.topic_name, info.part_name)
+
+	if info.option != node.Option {
+		z.zk.UpdatePartitionNode(zookeeper.PartitionNode{
+			TopicName: info.topic_name,
+			Name: info.part_name,
+			Option: info.option,
+			PTPoffset: node.PTPoffset,
+		})
+	}
+
+	switch info.option {
+	case -1:
+		if node.Option == -1 {  //与原来的状态相同
+			ret = "HadRaft" 
+		}
+		if node.Option == 1 || node.Option == 0 {	//原状态为fetch, 关闭原来的写状态, 创建新raft集群
+			//查询raft集群的broker, 发送信息
+			// 关闭raft集群, 开启fetch操作,不需要更换文件
+
+		}
+		if node.Option == -2 {  //未创建任何状态, 即该partition未接收过任何信息
+			//负载均衡获得一定数量broker节点,并在这些broker上部署raft集群 
+			var brokers []*server_operations.Client
+			for _, broker := range brokers {
+				broker.
+			}
+		}
+
+	default:
+		if node.Option != -1 {	//与原状态相同
+			ret = "HadFetch"
+		}else{          //由raft改为fetch
+			//查询fetch的Broker, 发送信息
+			//关闭fetch操作, 创建raft集群,需要更换文件
+
+		}	
+
+		if node.Option == -2 {  //未创建任何状态, 即该partition未接收过任何信息
+			//负载均衡获得一定数量broker节点,选择一个leader, 并让其他节点fetch leader信息
+		}
+
+	}
+	return Info_out{
+		Ret: ret,
 	}
 }
 
@@ -153,9 +214,8 @@ func (z *ZkServer)SubHandle(info Info_in) error {
 }
 
 //consumer查询该向那些broker发送请求
-//zkserver让broker准备好topic/sub和config
+//zkserver让broker准备好topic/sub和config    //Pub版
 func (z *ZkServer)HandStartGetBroker(info Info_in) (rets []byte, size int, err error) {
-	var partkeys []clients.PartKey
 	var Parts 	 []zookeeper.Part
 	/* 
 	检查该应户是否订阅了该topic/partition
@@ -168,9 +228,10 @@ func (z *ZkServer)HandStartGetBroker(info Info_in) (rets []byte, size int, err e
 		Option:	info.option,
 	})
 	
-	if info.option == 1 { //ptp
+	//获取该topic或partition的broker,并保证在线,若全部离线则Err
+	if info.option == 1 { //ptp_push
 		Parts, err = z.zk.GetBrokers(info.topic_name)
-	}else if info.option == 3 { //psb
+	}else if info.option == 3 { //psb_push
 		Parts, err = z.zk.GetBroker(info.topic_name, info.part_name, info.index)
 	}
 	if err != nil {
@@ -179,13 +240,37 @@ func (z *ZkServer)HandStartGetBroker(info Info_in) (rets []byte, size int, err e
 
 	//获取到信息后将通知brokers，让他们检查是否有该Topic/Partition/Subscription/config等
 	//并开启Part发送协程，若协程在超时时间到后未收到管道的信息，则关闭该协程
+	// var partkeys []clients.PartKey
+	// if info.option == 1 || info.option == 3 {
+	partkeys := z.SendPreoare(Parts, info)
+	// }else{
+		// partkeys = GetPartKeys(Parts)
+	// }
+
+	data, err := json.Marshal(partkeys)
+	if err != nil {
+		DEBUG(dError, "turn partkeys to json faile %v", err.Error())
+	}
+
+	return data, len(partkeys), nil
+}
+
+//push
+func (z *ZkServer)SendPreoare(Parts []zookeeper.Part, info Info_in) (partkeys []clients.PartKey){
+
 	for _, part := range Parts {
+		if part.Err != OK {
+			partkeys = append(partkeys, clients.PartKey{
+				Err: part.Err,
+			})
+			continue
+		}
 		z.mu.RLock()
 		bro_cli, ok := z.Brokers[part.BrokerName]
 		z.mu.RUnlock()
 
 		if !ok {
-			bro_cli, err = server_operations.NewClient(z.Name, client.WithHostPorts(part.Host_Port))
+			bro_cli, err := server_operations.NewClient(z.Name, client.WithHostPorts(part.Host_Port))
 			if err != nil {
 				DEBUG(dError, "broker(%v) host_port(%v) con't connect %v",part.BrokerName, part.Host_Port, err.Error())
 			}
@@ -213,15 +298,31 @@ func (z *ZkServer)HandStartGetBroker(info Info_in) (rets []byte, size int, err e
 			Name: part.Part_name,
 			Broker_name: part.BrokerName,
 			Broker_H_P: part.Host_Port,
+			Err: OK,
 		})
 	}
 
-	data, err := json.Marshal(partkeys)
-	if err != nil {
-		DEBUG(dError, "turn partkeys to json faile %v", err.Error())
-	}
+	return partkeys
+}
 
-	return data, len(partkeys), nil
+func (z *ZkServer)UpdateOffset(info Info_in) error {
+	err := z.zk.UpdatePartitionNode(zookeeper.PartitionNode{
+		Name: info.part_name,
+		TopicName: info.topic_name,
+		PTPoffset: info.index,
+	})
+	return err
+}
+
+func GetPartKeys(Parts []zookeeper.Part) (partkeys []clients.PartKey){
+	for _, part := range Parts  {
+		partkeys = append(partkeys, clients.PartKey{
+			Name: part.Part_name,
+			Broker_name: part.BrokerName,
+			Broker_H_P: part.Host_Port,
+		})
+	}
+	return partkeys
 }
 
 //发送请求到broker，关闭该broker上的partition的接收程序
