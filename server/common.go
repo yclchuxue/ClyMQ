228870675f27ee59c46e3b6338b73c74e648ef5d
yclchuxue <3136165538@qq.com>
2022-11-29 18:42:00 +0800

    add some api for test

--- server/common.go
+++ server/common.go
@@ -7,58 +7,58 @@ import (
 	"runtime"
 )
 
-type PartKey struct{
+type PartKey struct {
 	name string `json:"name"`
 }
 
 //初始化Broker时的信息
 type Options struct {
-	Name 				string
-	Tag 				string
-	Zkserver_Host_Port 	string
-	Broker_Host_Port 	string
-	Raft_Host_Port		string
+	Name               string
+	Tag                string
+	Zkserver_Host_Port string
+	Broker_Host_Port   string
+	Raft_Host_Port     string
 }
 
 //Broker向ZKServer发送自己的新能指标,用于按权值负载均衡
 type Property struct {
-	Name 		string 		`json:"name"`
-	Power		int64		`json:"power"`
-	CPURate		int64		`json:"cpurate"`
-	DiskIO 		int64		`json:"diskio"`
+	Name    string `json:"name"`
+	Power   int64  `json:"power"`
+	CPURate int64  `json:"cpurate"`
+	DiskIO  int64  `json:"diskio"`
 }
 
 //Broker启动时获得的初始信息，
-type BroNodeInfo struct{
-	Topics 		map[string]TopNodeInfo 	`json:"topics"`
+type BroNodeInfo struct {
+	Topics map[string]TopNodeInfo `json:"topics"`
 }
 
-type TopNodeInfo struct{
-	Topic_name 	string
-	Part_nums	int
-	Partitions 	map[string]ParNodeInfo
+type TopNodeInfo struct {
+	Topic_name string
+	Part_nums  int
+	Partitions map[string]ParNodeInfo
 }
 
-type ParNodeInfo struct{
-	Part_name 	string
-	Block_nums	int
-	Blocks 		map[string]BloNodeInfo
+type ParNodeInfo struct {
+	Part_name  string
+	Block_nums int
+	Blocks     map[string]BloNodeInfo
 }
 
-type BloNodeInfo struct{
-	Start_index 	int64
-	End_index		int64
-	Path 			string
-	File_name		string
+type BloNodeInfo struct {
+	Start_index int64
+	End_index   int64
+	Path        string
+	File_name   string
 }
 
 type BrokerS struct {
-	Brokers map[string]string 	`json:"brokers"`
+	Brokers map[string]string `json:"brokers"`
 }
 
 const (
-	ZKBROKER  	=	"zkbroker"
-	BROKER 		=	"broker"
+	ZKBROKER = "zkbroker"
+	BROKER   = "broker"
 )
 
 func GetIpport() string {
@@ -75,13 +75,12 @@ func GetIpport() string {
 }
 
 func CheckFileOrList(path string) (ret bool) {
-	_, err := os.Stat(path)    //os.Stat获取文件信息
+	_, err := os.Stat(path) //os.Stat获取文件信息
 	if err != nil {
 		return os.IsExist(err)
 	}
 	return true
 }
-
 
 func MovName(OldFilePath, NewFilePath string) error {
 	return os.Rename(OldFilePath, NewFilePath)
@@ -95,7 +94,7 @@ func CreateList(path string) error {
 		err := os.Mkdir(path, 0775)
 		if err != nil {
 			_, file, line, _ := runtime.Caller(1)
-			DEBUG(dError, "%v:%v mkdir %v error %v",file, line, path, err.Error())
+			DEBUG(dError, "%v:%v mkdir %v error %v\n", file, line, path, err.Error())
 		}
 	}
 
@@ -103,7 +102,7 @@ func CreateList(path string) error {
 }
 
 func CreateFile(path string) (file *os.File, err error) {
-	file, err = os.Create(path);
+	file, err = os.Create(path)
 
 	return file, err
 }
@@ -130,16 +129,15 @@ func GetPartKeyArray(parts map[string]*Partition) []PartKey {
 	return array
 }
 
-
 func CheckChangeCli(old map[string]*client_operations.Client, new []string) (reduce, add []string) {
 	for _, new_cli := range new {
-		if _, ok := old[new_cli]; !ok {  //new_cli 在old中没有
+		if _, ok := old[new_cli]; !ok { //new_cli 在old中没有
 			add = append(add, new_cli)
 		}
 	}
 
 	for old_cli := range old {
-		had := false   //不存在
+		had := false //不存在
 		for _, name := range new {
 			if old_cli == name {
 				had = true
@@ -147,9 +145,61 @@ func CheckChangeCli(old map[string]*client_operations.Client, new []string) (red
 			}
 		}
 		if !had {
-			reduce = append(reduce, old_cli) 
+			reduce = append(reduce, old_cli)
 		}
 	}
 
 	return reduce, add
+}
+
+
+//use for Test
+type Info struct {
+	// name       string //broker name
+	Topic_name string
+	Part_name  string
+	File_name  string
+	New_name   string
+	Option     int8
+	Offset     int64
+	Size       int8
+
+	Ack int8
+
+	Producer string
+	Consumer string
+	Cmdindex int64
+	// startIndex  int64
+	// endIndex  	int64
+	Message []byte
+
+	//raft
+	Brokers map[string]string
+	Me      int
+
+	//fetch
+	LeaderBroker string
+	HostPort     string
+}
+
+//use in test
+func GetInfo(in Info) info {
+	return info{
+		topic_name:   in.Topic_name,
+		part_name:    in.Part_name,
+		file_name:    in.File_name,
+		new_name:     in.New_name,
+		option:       in.Option,
+		offset:       in.Offset,
+		size:         in.Size,
+		ack:          in.Ack,
+		producer:     in.Producer,
+		consumer:     in.Consumer,
+		cmdindex:     in.Cmdindex,
+		message:      in.Message,
+		brokers:      in.Brokers,
+		me:           in.Me,
+		LeaderBroker: in.LeaderBroker,
+		HostPort:     in.HostPort,
+	}
 }
