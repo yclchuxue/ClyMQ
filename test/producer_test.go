4afc60725ae4f9afb5ff241d30d10627bb778344
yclchuxue <3136165538@qq.com>
2022-12-05 20:26:38 +0800

    add test

--- test/producer_test.go
+++ test/producer_test.go
@@ -38,3 +38,33 @@ func TestProducerCreate(t *testing.T) {
 	ShutDownZKServer(zkServer)
 	fmt.Println("  ... Passed")
 }
+
+func TestProducerPush(t *testing.T) {
+	fmt.Println("Test: producer Push message to Partition")
+
+	zkServer := StartZKServer(t)
+	time.Sleep(1*time.Second)
+
+	brokers := StartBrokers(t, 1)
+	time.Sleep(1*time.Second)
+
+	producer := NewProducerAndStart(t, ":7878", "producer1")
+	time.Sleep(1*time.Second)
+
+
+	fmt.Println("Producer Create a Topic")
+	err := producer.CreateTopic("phone_number")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	fmt.Println("Producer Create a Topic/Partition")
+	err = producer.CreatePart("phone_number", "xian")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	ShutDownBrokers(brokers)
+	ShutDownZKServer(zkServer)
+	fmt.Println("  ... Passed")
+}
