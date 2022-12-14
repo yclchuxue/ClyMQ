d8362c47f06522d434dec73eb0e3019b8b27843b
yclchuxue <3136165538@qq.com>
2022-12-14 19:06:54 +0800

    add some code

--- test/file_test.go
+++ test/file_test.go
@@ -148,3 +148,65 @@ func TestFile2(t *testing.T) {
 
 	fmt.Println("   ... Passed")
 }
+
+func TestFile3(t *testing.T) {
+	fmt.Println("Test: File GetFirstIndex and GetIndex")
+	// server.Name = "Broker"
+	topic_name := "phone_number"
+	part_name := "xian"
+	filename := "NowBlock.txt"
+	messages := []string{"18700619719", "1234567891",
+		"12345678911", "12345678912",
+		"12345678913", "12345678914",
+		"12345678915", "12345678916",
+		"12345678917", "12345678918",
+		"12345678919"}
+	//删除文件夹下所有文件
+	str, _ := os.Getwd()
+	DeleteAllFiles(str + "/" + "Broker" + "/" + topic_name + "/" + part_name + "/", t)
+
+	path := str + "/" + "Broker" + "/" + topic_name + "/" + part_name + "/" + filename
+	file, fd, Err, err := server.NewFile(path)
+
+	if err != nil {
+		t.Fatal(Err, err.Error())
+	}
+	index := int64(11)
+	var msgs []server.Message
+	node := server.Key{
+		Start_index: index,
+	}
+
+	for _, msg := range messages {
+		msgs = append(msgs, server.Message{
+			Index: index,
+			Size:  1,
+			Topic_name: topic_name,
+			Part_name: part_name,
+			Msg: []byte(msg),
+		})
+		index++
+	}
+
+	data_msg, err := json.Marshal(msgs)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	node.End_index = index-1
+	node.Size = int64(len(data_msg))
+
+	fmt.Println("---WriteFile node and data_msg")
+	file.WriteFile(fd, node, data_msg)
+
+	first_index := file.GetFirstIndex(fd)
+	if first_index != 11 {
+		t.Fatal("   --- the first_index ", first_index, " != 11")
+	}
+
+	end_index := file.GetIndex(fd)
+	if end_index != node.End_index {
+		t.Fatal("   --- the end_index ", end_index, " != ", node.End_index)
+	}
+
+	fmt.Println("   ... Passed")
+}
--- test/init_test.go
+++ test/init_test.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"ClyMQ/client/clients"
+	"ClyMQ/server"
 	"fmt"
 	"testing"
 	"time"
 )
 
 //测试前我们需要对zookeeper和各个集群进行初始化一些数据方便后续的测试
-func TestInit(t *testing.T) {
+func TestInit1(t *testing.T) {
 	fmt.Println("Init: init status for test")
 
 	messages := []string{"18700619719", "1234567891",
@@ -27,14 +28,14 @@ func TestInit(t *testing.T) {
 	producer := NewProducerAndStart(t, ":7878", "producer1")
 	time.Sleep(1 * time.Second)
 
-	//创建三个topic
+	//创建topic
 	fmt.Println("Producer Create a Topic")
 	err := producer.CreateTopic("phone_number")
 	if err != nil {
 		t.Fatal(err.Error())
 	}
 
-	//每个topic创建三个partition
+	//每个topic创建partition
 	fmt.Println("Producer Create a Topic/Partition")
 	err = producer.CreatePart("phone_number", "xian")
 	if err != nil {
@@ -60,6 +61,57 @@ func TestInit(t *testing.T) {
 			t.Fatal(err.Error())
 		}
 	}
+
+	time.Sleep(time.Second * 30)
+	//向partitions中生产一些信息
+
+	ShutDownBrokers(brokers)
+	ShutDownZKServer(zkServer)
+	fmt.Println("  ... Init Successful")
+}
+
+func TestInit2(t *testing.T) {
+	fmt.Println("Init: init status for test")
+
+	// messages := []string{"18700619719", "1234567891",
+	// 	"12345678911", "12345678912",
+	// 	"12345678913", "12345678914",
+	// 	"12345678915", "12345678916",
+	// 	"12345678917", "12345678918",
+	// 	"12345678919"}
+
+	zkServer := StartZKServer(t)
+	time.Sleep(1 * time.Second)
+
+	brokers := StartBrokers(t, 3)
+	time.Sleep(1 * time.Second)
+
+	consumer := NewConsumerAndStart(t, ":7881", ":7878", "consumer1")
+
+	time.Sleep(1 * time.Second)
+
+	parts, ret, err := consumer.StartGet(clients.Info{
+		Topic: "phone_number",
+		Part: "xian",
+		Option: server.PTP_PULL,
+	})
+	if err != nil {
+		t.Fatal(ret, err.Error())
+	}
+
+	for _, part := range parts {
+		cli, err := consumer.GetCli(part)
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+		info := clients.NewInfo(0, "phone_number", "xian")
+		info.Cli = cli
+		start, end, msgs, err := consumer.Pull(info)
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+		fmt.Println("start ", start, "end ", end, "msgs is ", msgs)
+	} 
 
 	time.Sleep(time.Second * 30)
 	//向partitions中生产一些信息
