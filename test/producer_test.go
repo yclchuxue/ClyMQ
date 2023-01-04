be7f838dfce12b4e008f3390a17ed334ff7ff301
yclchuxue <3136165538@qq.com>
2023-01-04 21:23:27 +0800

    update test

--- test/producer_test.go
+++ test/producer_test.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"ClyMQ/client/clients"
 	"fmt"
 
 	"testing"
 	"time"
-
 )
 
 func TestProducerCreate(t *testing.T) {
@@ -42,27 +42,58 @@ func TestProducerCreate(t *testing.T) {
 func TestProducerPush(t *testing.T) {
 	fmt.Println("Test: producer Push message to Partition")
 
-	zkServer := StartZKServer(t)
-	time.Sleep(1*time.Second)
+	messages := []string{"18700619719", "1234567891",
+		"12345678911", "12345678912",
+		"12345678913", "12345678914",
+		"12345678915", "12345678916",
+		"12345678917", "12345678918",
+		"12345678919"}
 
-	brokers := StartBrokers(t, 1)
-	time.Sleep(1*time.Second)
+	zkServer := StartZKServer(t)
+	time.Sleep(1 * time.Second)
+
+	brokers := StartBrokers(t, 3)
+	time.Sleep(1 * time.Second)
 
 	producer := NewProducerAndStart(t, ":7878", "producer1")
-	time.Sleep(1*time.Second)
+	time.Sleep(1 * time.Second)
 
-
+	//创建三个topic
 	fmt.Println("Producer Create a Topic")
 	err := producer.CreateTopic("phone_number")
 	if err != nil {
 		t.Fatal(err.Error())
 	}
 
+	//每个topic创建三个partition
 	fmt.Println("Producer Create a Topic/Partition")
 	err = producer.CreatePart("phone_number", "xian")
 	if err != nil {
 		t.Fatal(err.Error())
 	}
+
+	//将partition设置状态
+	err = producer.SetPartitionState("phone_number", "xian", -1, 3)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	//等待领导者选出
+	time.Sleep(time.Second * 5)
+
+	for _, message := range messages {
+		err = producer.Push(clients.Message{
+			Topic_name: "phone_number",
+			Part_name:  "xian",
+			Msg:        []byte(message),
+		}, -1)
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	}
+
+	time.Sleep(time.Second * 30)
+	//向partitions中生产一些信息
 
 	ShutDownBrokers(brokers)
 	ShutDownZKServer(zkServer)
